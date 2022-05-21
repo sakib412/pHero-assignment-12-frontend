03e0ec14b2767371801bdf8278427b3dd2713d5c
Najmus Sakib <nazmusakib412@gmail.com>
2022-05-22 02:22:25 +0600

    [ADDED] carousel

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Carousel from '../../components/Carousel/Carousel'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Carousel />
+        </div>
     )
 }
 
